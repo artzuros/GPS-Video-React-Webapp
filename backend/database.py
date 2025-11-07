b58ba09d5d553b585e968f199388d9258a644417
artzuros <gunubansal129@gmail.com>
2025-11-07 04:50:01 +0000

    DB Updated for 3.21

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:admin@localhost/rah_db"
+DATABASE_URL = "postgresql://admin:admin@mord-1-0-db:5432/rahi"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
